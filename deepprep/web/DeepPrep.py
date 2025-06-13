5d3beff862c29d072a4a8a80ce02f9ed08b5ef5b
Ning AN <ninganme0317@gmail.com>
2025-06-13 10:07:18 +0800

    add rest/denoise

--- deepprep/web/DeepPrep.py
+++ deepprep/web/DeepPrep.py
@@ -42,10 +42,6 @@ st.page_link("pages/3_📝Quick QC.py",
     label="👉Quick QC of T1w & BOLD🔗"
 )
 
-st.page_link("pages/4_🎯Target Auto Planing.py",
-    label="👉Target Auto Planing🔗"
-)
-
 st.markdown(
     """
     -----------------
